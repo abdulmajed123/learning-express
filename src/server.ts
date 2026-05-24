3c449d5116608dbda6008e61006359476f4b9a6c
abdulmajed123 <abdulmajed5618@gmail.com>
2026-05-24 17:07:50 +0600

    setup express server with typescript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { type Application } from "express";
+const app: Application = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
