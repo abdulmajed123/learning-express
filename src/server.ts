68d42b9dc5e5ea1d7bf3df7686d992ecadefaecd
abdulmajed123 <abdulmajed5618@gmail.com>
2026-05-30 09:01:48 +0600

    setup postgres pool and create users table

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,30 @@ const pool = new Pool({
   connectionString:
     "postgresql://neondb_owner:npg_OAx9PGW2mqwc@ep-autumn-star-apqxi040-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (20),
+      email VARCHAR (20) NOT NULL,
+      password VARCHAR (20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+
+      )
+      `);
+    console.log("Database Create Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
 app.get("/", (req: Request, res: Response) => {
   // res.send("Hello World!");
   res.status(200).json({
