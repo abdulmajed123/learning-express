d7bf69de50cc2fd7db89fb3222b9329ddebe4dc2
abdulmajed123 <abdulmajed5618@gmail.com>
2026-06-06 10:34:55 +0600

    implement user profile creation

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,5 +1,21 @@
 import type { Request, Response } from "express";
+import { profileRoute } from "./profile.route.js";
+import { profileService } from "./profile.service.js";
 
-const createProfile = async (req: Request, res: Response) => {};
-
+const createProfile = async (req: Request, res: Response) => {
+  try {
+    const result = await profileService.createProfileIntoDB(req.body);
+    res.status(201).json({
+      success: true,
+      message: "Profile Create Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
 export const profileController = { createProfile };
